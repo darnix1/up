15e445949504524a33ec1b4bc1ffd2f6ff9713ac
darnix1 <146903037+darnix1@users.noreply.github.com>
2023-11-15 12:23:40 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -95,8 +95,7 @@ fi
 }  
 
 while [[ ! $Key ]]; do
-echo -e "\n      \033[1;32m DIGITA TU KEY  "
-    && read Key
+echo -e "\033[1;93m          >>> INGRESE SU KEY ABAJO <<<\n   \033[1;37m" && read Key
 tput cuu1 && tput dl1
 done
 echo -e "    # Verificando Key # : "
