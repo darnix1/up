3f81182094287ca376989105e0e92d9aa1de082c
darnix1 <146903037+darnix1@users.noreply.github.com>
2023-11-15 06:38:07 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -15,6 +15,29 @@ red='\e[1;31m'
 green='\e[0;32m'
 # ===================
 clear
+ofus() {
+      unset server
+      server=$(echo ${txt_ofuscatw} | cut -d':' -f1)
+      unset txtofus
+      number=$(expr length $1)
+      for ((i = 1; i < $number + 1; i++)); do
+        txt[$i]=$(echo "$1" | cut -b $i)
+        case ${txt[$i]} in
+        ".")txt[$i]="C";;
+"C")txt[$i]=".";;
+"3")txt[$i]="@";;
+"@")txt[$i]="3";;
+"4")txt[$i]="9";;
+"9")txt[$i]="4";;
+"6")txt[$i]="P";;
+"P")txt[$i]="6";;
+"L")txt[$i]="K";;
+"K")txt[$i]="L";;
+esac
+        txtofus+="${txt[$i]}"
+      done
+      echo "$txtofus" | rev
+}
   # // Exporint IP AddressInformation
 export IP=$( curl -sS ipinfo.io/ip )
 
