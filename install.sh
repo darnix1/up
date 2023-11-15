1757067f2b5080cb6e45ee65dca5151c65a16c07
darnix1 <146903037+darnix1@users.noreply.github.com>
2023-11-15 07:30:32 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -68,12 +68,7 @@ while [[ ! $Keey ]]; do
         tput cuu1 && tput dl1
     done
 REQUEST=$(ofus "$Keey" | cut -d'/' -f2)
-    echo -e "\n"
-    echo -e " FILE Contend : ${REQUEST} $(echo ${REQUEST} | wc -c)"
-    echo -e "\n"
-    echo -e " VERIFICA, Si tu key Contiene \033[1;45m KEY DE ChumoGH! \033[0m "
-    echo -e "\n"
-    echo -e " Link Key: http://$(ofus $Keey) \n                      "
+    echo -e "\n"                      "
     IiP=$(ofus "$Keey" | grep -vE '127\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | grep -o -E '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}')
     [[ $(curl -s --connect-timeout 2 $IiP:8888) ]] && echo -e "\033[1;42mCONEXION CON SERVIDOR EXITOSA\033[0m" || echo -e "\033[1;43mCONEXION CON SERVIDOR FALLIDA\033[0m"
     wget --no-check-certificate -O $HOME/list-key $(ofus $Keey)/$(wget -qO- ipv4.icanhazip.com) >/dev/null 2>&1 && echo -ne "\033[1;32m  [ VERIFICANDO ]" || echo -e "\033[1;31m [ No Existe Acceso al KEY ]" #&& echo -e "\033[1;32m [ Key  ]\n" || echo -e "\033[1;31m [ No Existe Acceso al KEY ]"
@@ -94,27 +89,26 @@ REQUEST=$(ofus "$Keey" | cut -d'/' -f2)
         pontos+="."
         n=$(($n + 1))
     done
-    echo -e " ---> ESTADO : \033[1;45m$ofen\033[0m  con "
+    echo -e " ---> ESTADO : \033[1;45m$ofen\033[0m "
     [[ ! -e $HOME/install/log.txt ]] && touch $HOME/install/log.txt
-    echo " $(cat <$HOME/install/log.txt | wc -l) FILES " && rm -f $HOME/install/log.txt
+    echo " $(cat <$HOME/install/log.txt | wc -l) " && rm -f $HOME/install/log.txt
     echo -e " \033[1;42mESTADO :\033[0m "
     [[ -e $HOME/list-key ]] && {
         echo -e "  "
-        [[ $ofen = "KEY DE ChumoGH!" ]] &&
+        [[ $ofen = "KEY INVALIDA!" ]] &&
             echo -e "KEY FUNCIONAL" && rm -f $HOME/list-key && echo -ne "\033[0m"
-    } || echo -e " KEY INVALIDA O USADA\033[0m\n"
+    } || echo -e " KEY INVALIDA \033[0m\n"
     curl -s --connect-timeout 2 ${IiP}:81/${REQUEST}/menu_credito > menu_credito
-    echo -e " RESELLER del Key :\033[0m  "
     [[ -e $HOME/install/menu_credito ]] && {
         echo -e "  "
         [[ "$(cat $HOME/install/menu_credito)" = "" ]] && {
-            echo -e "SIN RESELLER\033[0m"
+            
         } || echo -e "$(cat $HOME/install/menu_credito)\033[0m" && rm -rf $HOME/install && echo -ne "\033[0m"
-    } 
+    }
     
 # Continúa con el resto del script después de que se haya ingresado un UUID válido
 #echo "Continuando con el script..."
-    echo -e " \e[3;32m AUTORIZANDO IP DE USUARIO\e[0m" | pv -qL 10 ; rm $_Ink/list > /dev/null 2>&1; wget -P $_Ink https://raw.githubusercontent.com/DanssBot/SSHPLUS/main/Install/list >/dev/null 2>&1
+    echo -e " \e[3;32m AUTORIZANDO IP DE USUARIO\e[0m" | pv -qL 10 ; rm $_Ink/list > /dev/null 2>&1
     
     sleep 5s
     clear
