64f572138633d856848ea7c533b2c4951c5396a8
darnix1 <146903037+darnix1@users.noreply.github.com>
2023-11-21 22:10:46 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,10 +2,7 @@
 down=($(ifconfig $(ip route | grep default | awk '{print $5}') | grep -o "(.*)" ))
 download_down=$(echo ${down[1]}${down[2]} | sed -e 's/(//' |sed -e 's/)//')
 download_up=$(echo ${down[3]}${down[4]} | sed -e 's/(//' |sed -e 's/)//')
-echo -e " DESCARGA : $download_down | SUBIDA : $download_up" |column -t -s '|'
+echo -e "\033[1;37m DESCARGA \033[1;32m $download_down | \033[1;37m SUBIDA \033[1;32m $download_up" |column -t -s '|'
 
 
-echo -e "\033[1;37m DESCARGA ↡ \033[1;32m --> \033[1;34m " $download_down "\033[1;32m <--\033[1;37m Ethernet ⇧ "
-
-echo -e "\033[1;37m SUBIDA ↡ \033[1;32m --> \033[1;34m " $download_up "\033[1;32m <--\033[1;37m Ethernet ⇧ "
 
