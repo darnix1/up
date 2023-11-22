d9a91b36e26031cf03d66158288a9a328a56b330
darnix1 <146903037+darnix1@users.noreply.github.com>
2023-11-21 22:24:26 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ echo -e "\033[1;37m DESCARGA \033[1;32m $download_down | \033[1;37m SUBIDA \033[
 echo -e " $COLOR1╭═════════════════════════════════════════════════╮${NC}"
 echo -e " $COLOR1│$NC${WH} Version$NC       ${WH}=$NC $a $stl"  
 echo -e " $COLOR1│$NC${WH} Usuario$NC       ${WH}=$NC $author $sts"
-echo -e " $COLOR1│$NC${WH} \033[1;37m DESCARGA \033[1;32m $download_down | \033[1;37m SUBIDA \033[1;32m $download_up" |column -t -s '|'
+echo -e " $COLOR1│$NC${WH} \033[1;37mDESCARGA \033[1;32m $download_down | \033[1;37m SUBIDA \033[1;32m $download_up" |column -t -s '|'
 echo -e " $COLOR1╰═════════════════════════════════════════════════╯${NC}"
 echo
 
