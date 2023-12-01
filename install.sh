a54f57749c01a5e541bb6fb76a4fae45085b1345
darnix1 <146903037+darnix1@users.noreply.github.com>
2023-12-01 12:23:49 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 
-# Paso 01
+# Verificar el sistema operativo
+if [ -f /etc/debian_version ]; then
+    # Paso 01 para Debian
+    sudo apt-get update
+    sudo apt-get install -y build-essential libsqlite3-dev zlib1g-dev libncurses5-dev libgdbm-dev libbz2-dev libreadline5-dev libssl-dev libdb-dev make
 
-sudo apt-get install -y build-essential libsqlite3-dev zlib1g-dev libncurses5-dev libgdbm-dev libbz2-dev libreadline5-dev libssl-dev libdb-dev
-sudo apt-get install make
-export PATH=$PATH:/etc/Python-2.7.3
-wget http://www.python.org/ftp/python/2.7.3/Python-2.7.3.tgz
-sudo mv Python-2.7.3.tgz /etc
-cd /etc
-sudo tar -xzf Python-2.7.3.tgz
+    # Paso 02
+    wget http://www.python.org/ftp/python/2.7.3/Python-2.7.3.tgz
+    sudo mv Python-2.7.3.tgz /usr/src
+    cd /usr/src
+    sudo tar -xzf Python-2.7.3.tgz
 
-# Paso 03
-tar -xzf Python-2.7.3.tgz
+    # Paso 03
+    cd Python-2.7.3
 
-# Paso 04
-cd Python-2.7.3
+    # Paso 04
+    ./configure --prefix=/usr --enable-shared
+fi
 
-# Paso 05
-./configure --prefix=/usr --enable-shared
+# Resto de las instrucciones
+# ...
 
+#echo "El script ha completado su ejecución."
