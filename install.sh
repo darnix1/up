8b4280a633f29a86b2e8b1d852dd4e1910d0a09c
darnix1 <146903037+darnix1@users.noreply.github.com>
2023-12-01 12:16:58 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 
 sudo apt-get install -y build-essential libsqlite3-dev zlib1g-dev libncurses5-dev libgdbm-dev libbz2-dev libreadline5-dev libssl-dev libdb-dev
 sudo apt-get install make
-export PATH=$PATH:/usr/bin
+export PATH=$PATH:/etc/Python-2.7.3
 wget http://www.python.org/ftp/python/2.7.3/Python-2.7.3.tgz
 sudo mv Python-2.7.3.tgz /etc
 cd /etc
