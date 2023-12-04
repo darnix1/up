7f910f19b682da076225bc78b9f455d4799c17a1
darnix1 <146903037+darnix1@users.noreply.github.com>
2023-12-03 21:41:33 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,8 @@
-#!/bin/sh
-
-Key="qraLatsil/05@536f?9e09?7b/8888:@5v87@v03?v75@"
-result=$(curl -sSL -H "Ofus: $ofus" -H "Key: $Key" 157.230.178.51:81/dani/checkIP.log | grep "$Key")
+result=$(curl -sSL -H "Ofus: $ofus" -H "Key: $key" 157.230.178.51:81/dani/checkIP.log | grep "$key")
 
 if [ -n "$result" ]; then
-    ip_columna_3=$(echo "$result" | awk -F'|' '{print $3}' | tr -d '[:space:]')
-    echo "IP en la columna 3: $ip_columna_3"
+    column_value=$(echo "$result" | awk 'NR==1{print $3}')
+    echo "Valor de la columna 2: $column_value"
 else
     echo "No se encontró ninguna coincidencia para la Key ingresada."
 fi
